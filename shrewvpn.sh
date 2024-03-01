11aa0572ef98f59805f4f0aba828131e4a7fd16e
Joelson Salustiano <86619225+joelsondepaula@users.noreply.github.com>
2024-03-01 08:44:49 -0300

    Create shrewvpn.sh

---
+++ shrewvpn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+user=$(whoami)
+
+cd  /home
+
+sudo apt update
+
+sudo apt install build-essential \
+qtcreator \
+qt5-default qt5-qmake \
+cmake libssl-dev libedit-dev flex bison \
+
+sudo git clone https://github.com/HayfaAbusnina/ike.git
+
+sed -i "s/habusnina/$user/g" /home/ike/source/qikea/moc_qikea.cpp_parameters
+sed -i "s/habusnina/$user/g" /home/ike/source/qikec/moc_qikec.cpp_parameters
+
+cd /home/ike/; cmake -DCMAKE_INSTALL_PREFIX=/usr -DQTGUI=YES -DETCDIR=/etc -DNATT=YES; make; sudo make install; sudo ldconfig; sudo mv /etc/iked.conf.sample /etc/iked.conf
