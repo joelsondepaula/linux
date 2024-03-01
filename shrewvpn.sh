e07c4b31cb6891a98a7265b14b7dedae3186f8ff
Joelson Salustiano <86619225+joelsondepaula@users.noreply.github.com>
2024-03-01 11:11:54 -0300

    Update shrewvpn.sh

--- shrewvpn.sh
+++ shrewvpn.sh
@@ -3,21 +3,23 @@ user=$(whoami)
 
 cd  /home
 
-sudo apt update -y
+sudo apt update
 
-sudo apt install -y build-essential \
+sudo apt install build-essential \
 qtcreator \
 qt5-default qt5-qmake \
 cmake libssl-dev libedit-dev flex bison \
-git \
 
-sudo git clone https://github.com/HayfaAbusnina/ike.git; sudo sed -i "s/habusnina/$user/g" /home/ike/source/qikea/moc_qikea.cpp_parameters; sudo sed -i "s/habusnina/$user/g" /home/ike/source/qikec/moc_qikec.cpp_parameters
+sudo git clone https://github.com/HayfaAbusnina/ike.git
 
-cd /home/ike; cmake -DCMAKE_INSTALL_PREFIX=/usr -DQTGUI=YES -DETCDIR=/etc -DNATT=YES; make; sudo make install; sudo ldconfig; sudo mv /etc/iked.conf.sample /etc/iked.conf
+sudo sed -i "s/habusnina/$user/g" /home/ike/source/qikea/moc_qikea.cpp_parameters
+sudo sed -i "s/habusnina/$user/g" /home/ike/source/qikec/moc_qikec.cpp_parameters
+
+cd /home/ike/; sudo cmake -DCMAKE_INSTALL_PREFIX=/usr -DQTGUI=YES -DETCDIR=/etc -DNATT=YES; sudo make; sudo make install; sudo ldconfig; sudo mv /etc/iked.conf.sample /etc/iked.conf
 
 echo "alias qikea='screen -S qikea -dm bash -c \"qikea; exec bash\"'" >> ~/.bashrc; source ~/.bashrc
 
 echo "Para executar a GUI basta rodar os comandos \"sudo iked\" e posteriormente \"qikea\""
-echo -e "\nArtigo com as instruções de instalação e configuração: https://hostdime.atlassian.net/wiki/spaces/Suporte/pages/131530998/Instala+o+e+configura+o+do+ShrewVPN+no+Ubuntu+para+conex+o+VPN+DialUP"
+echo -e "Artigo com as instruções de instalação e configuração: https://hostdime.atlassian.net/wiki/spaces/Suporte/pages/131530998/Instala+o+e+configura+o+do+ShrewVPN+no+Ubuntu+para+conex+o+VPN+DialUP"
 
 exit;
