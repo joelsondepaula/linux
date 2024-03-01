316b70f0133ca1e5c7b5a286d466254ea4d69e35
Joelson Salustiano <86619225+joelsondepaula@users.noreply.github.com>
2024-03-01 10:28:52 -0300

    Update shrewvpn.sh

--- shrewvpn.sh
+++ shrewvpn.sh
@@ -19,3 +19,8 @@ sed -i "s/habusnina/$user/g" /home/ike/source/qikec/moc_qikec.cpp_parameters
 cd /home/ike/; cmake -DCMAKE_INSTALL_PREFIX=/usr -DQTGUI=YES -DETCDIR=/etc -DNATT=YES; make; sudo make install; sudo ldconfig; sudo mv /etc/iked.conf.sample /etc/iked.conf
 
 echo "alias qikea='screen -S qikea -dm bash -c \"qikea; exec bash\"'" >> ~/.bashrc; source ~/.bashrc
+
+echo "Para executar a GUI basta rodar os comandos \"sudo iked\" e posteriormente \"qikea\""
+echo "Artigo com as instruções de configuração: https://hostdime.atlassian.net/wiki/spaces/Suporte/pages/131530998/Instala+o+e+configura+o+do+ShrewVPN+no+Ubuntu+para+conex+o+VPN+DialUP"
+
+exit;
